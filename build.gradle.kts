cc7d7b32db0b8a58f81d0e2e2d98f0f086dfa8d3
Elrol <elrol.eyno@gmail.com>
2025-05-04 15:05:03 -0500

    Updated ignore file

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+import org.codehaus.groovy.runtime.DefaultGroovyMethods.mixin
+
+plugins {
+    id("java")
+    id("architectury-plugin") version("3.4-SNAPSHOT")
+    id("dev.architectury.loom") version("1.7-SNAPSHOT")
+
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "dev.elrol.arrow.legends"
+version = "1.5.7"
+
+architectury {
+    platformSetupLoomIde()
+    fabric()
+}
+
+loom {
+    silentMojangMappingsLicense()
+
+    mixin {
+        defaultRefmapName.set("mixins.${project.name}.refmap.json")
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://maven.nucleoid.xyz")
+    maven(url = "https://maven.tomalbrc.de")
+    maven(url = "https://dl.cloudsmith.io/public/geckolib3/geckolib/maven/")
+    maven(url = "https://maven.impactdev.net/repository/development/")
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    minecraft ("com.mojang:minecraft:1.21.1")
+    mappings ("net.fabricmc:yarn:1.21.1+build.3:v2")
+    modImplementation("net.fabricmc:fabric-loader:0.16.10")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:0.104.0+1.21.1")
+    modImplementation(fabricApi.module("fabric-command-api-v2", "0.104.0+1.21.1"))
+
+    modImplementation("eu.pb4:polymer-core:0.9.18+1.21.1")
+    modImplementation("eu.pb4:polymer-resource-pack:0.9.18+1.21.1")
+    modImplementation("eu.pb4:polymer-autohost:0.9.18+1.21.1")
+    modImplementation("de.tomalbrc:filament:0.14.4+1.21.1")
+
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.12.3+kotlin.2.0.21")
+    modImplementation("eu.pb4:sgui:1.6.1+1.21.1")
+    modImplementation("com.cobblemon:fabric:1.6.0+1.21.1-SNAPSHOT")
+    modImplementation(files("libs/ArrowCore-1.5.0.jar"))
+    modImplementation(files("libs/ArrowCommands-1.5.1.jar"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+
+    implementation("com.mysql", "mysql-connector-j","9.2.0")
+
+    modImplementation("net.impactdev.impactor.api:economy:5.3.0")
+
+    compileOnly("net.luckperms:api:5.4")
+    runtimeOnly("net.luckperms:api:5.4")
+    modImplementation("me.lucko:fabric-permissions-api:0.3.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand(project.properties)
+    }
+}
+
+fabricApi {
+    configureDataGeneration()
+}
